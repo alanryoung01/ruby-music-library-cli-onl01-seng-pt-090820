29e9d120bc59e3a14a8cb413b7ebb437462eee19
Alan Young <alanryoung01@gmail.com>
2020-10-16 01:12:43 +0000

    Automatically backed up by Learn

---
+++ lib/Artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+  
