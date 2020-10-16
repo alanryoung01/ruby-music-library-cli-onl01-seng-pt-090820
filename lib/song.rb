fff5ce9125f5c42a9390a0e05a2e05be975bb012
Alan Young <alanryoung01@gmail.com>
2020-10-16 01:00:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,4 +42,20 @@ class Song
     all.detect{|save| save.name == name}
   end
 
-  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    song_name, artist_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{|save| s.save}
+  end
+end
